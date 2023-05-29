3520d278817e6563d65edad4285ed223fd58d131
Ryan Masango <ryanmasango@Ryans-MacBook-Air.local>
2023-05-29 14:52:07 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+$(document).on('ready', function() {
+    
+    $(".regular").slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+    
+  });
+
